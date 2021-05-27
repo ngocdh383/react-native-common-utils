82e1b783dba2a5ae50bc4553f693036f83c97cc9
Dinh Hong Ngoc <dinhhongngoc383@gmail.com>
2021-05-27 10:24:16 +0700

    remove constants and functions .d.ts

--- functions.d.ts
+++
@@ -1,2 +0,0 @@
-export function getScale(): 1 | 2 | 3;
-export function isAndroid(): boolean;
--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,6 @@ export const DEVICE_HEIGHT: number;
 
 export const SCREEN_WIDTH: number;
 export const SCREEN_HEIGHT: number;
+
+export function getScale(): 1 | 2 | 3;
+export function isAndroid(): boolean;
